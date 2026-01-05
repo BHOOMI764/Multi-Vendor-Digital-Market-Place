dc3d632904add028aef9b76713c424856fa0d9ab
Bhoomi <bhoomicat2005@gmail.com>
2026-01-05 14:48:36 +0530

    chore: add Heroku Procfile, runtime, .gitignore and whitenoise settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&mm%!zar*d_o@r=%(lhcfl0#dx+t9yk&0+lh8q$^0dozge6&w&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&mm%!zar*d_o@r=%(lhcfl0#dx+t9yk&0+lh8q$^0dozge6&w&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,8 +83,8 @@ DATABASES = {
     }
 }
 
-STRIPE_SECRET_KEY ='sk_test_51KSE6JSFdtVNX2gp6kU17LaliMGqgTwV7dl9cmnOjpwZj6XxTbfrLlpbPGIZWRk37oJ1XmfRmSBG38uqAtIXOdJ400loXDDGhn'
-STRIPE_PUBLISHABLE_KEY ='pk_test_51KSE6JSFdtVNX2gpCZQhnHQeQrbwIOBh6X31GR2mYf2lA5RyzVdlAzX3hXPwZZHr5SiLvWHTD3RllZ3VOIY5Jv7R00Vk9aa98y'
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY', '')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
