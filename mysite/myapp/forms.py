70ff70e0c7d0033b13c811b5450e3ccfcb0e9ca9
Bhoomi <bhoomicat2005@gmail.com>
2026-01-05 17:08:33 +0530

    fix: validate registration passwords and unique email

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -15,9 +15,18 @@ class UserRegistrationForm(forms.ModelForm):
         fields =['username','email','first_name']
     
     def check_password(self):
-        if self.cleaned_data['password']!=self.cleaned_data['password2']:
+    def clean_password2(self):
+        pw1 = self.cleaned_data.get('password')
+        pw2 = self.cleaned_data.get('password2')
+        if pw1 and pw2 and pw1 != pw2:
             raise forms.ValidationError('Password fields do not match')
-        return self.cleaned_data['password2']
+        return pw2
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError('A user with that email already exists')
+        return email
     
         
         
